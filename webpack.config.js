9fdce7b4c7cd513fd51cfc698149ca4b7387ddd1
Jonathan Zarate <zaratedev@gmail.com>
2018-02-06 17:05:23 -0600

    Initial commit and config basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+const htmlWebpack = new HtmlWebpackPlugin({
+	template: './assets/index.template.html',
+	filename: 'index.html'
+});
+
+module.exports = {
+	entry: './assets/js/entry.js',
+	output: {
+		publicPath: '/',
+		path: path.join(__dirname, '..'),
+		filename: 'dist/js/bundle.js'
+	},
+	plugins: [
+		htmlWebpack
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	}
+}
