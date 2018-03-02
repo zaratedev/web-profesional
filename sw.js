94463910ada5cbfcb994dc12e5fd2729a1a42573
zaratedev <zaratedev@gmail.com>
2018-03-01 23:04:34 -0600

    sends of files to the cache

--- sw.js
+++ sw.js
@@ -3,10 +3,11 @@ const CACHE_NAME = 'ZARATEDEV_CACHE-v1'
 self.addEventListener('install', function() {
   // Save files initiales
   caches.open(CACHE_NAME).then(function (cache) {
-    cache.addAll(['/index.html']);
+    cache.addAll(['/index.html', '/dist/js/bundle.js', '/public/images/1.jpeg']);
   })
 });
 
+// Eliminar el cache del service worker antigua y lo remplaza
 self.addEventListener('activate', function(ev) {
   ev.waitUntil(
     caches.keys().then( function(cacheNames) {
@@ -18,5 +19,36 @@ self.addEventListener('activate', function(ev) {
       return Promise.all( promises );
     })
   );
-
 });
+
+//Envio de archivos desde el cache
+self.addEventListener('fetch', function(ev) {
+  ev.respondWith(
+    caches.match(ev.request)
+      .then( function(response) {
+        return searchInCacheOrMarkeRequest(ev.request);
+      })
+      .catch( function(err) {
+        if ( ev.request.mode == "navigate")
+          return caches.match(ev.request);
+      })
+  );
+});
+
+function searchInCacheOrMarkeRequest( request ) {
+  const cachePromise = caches.open(CACHE_NAME);
+  const matchPromise = cachePromise.then( function(cache) {
+    return cache.match( request );
+  });
+
+  return Promise.all([cachePromise, matchPromise]).then( function([cache, cacheResponse]) {
+    const fetchPromise = fetch(request).then( function( fetchResponse ) {
+      cache.put(request, fetchResponse.clone());
+
+      return fetchResponse;
+    });
+
+    return cacheResponse || fetchPromise;
+
+  })
+}
